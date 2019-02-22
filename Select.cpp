f64bf100dec8a12055fa972e2b079bfeb45fd664
Jingren Li <>
2019-02-22 09:44:19 +0000

    fixed the bug with empty data; Better visualisation for the output reseult.

--- Select.cpp
+++ Select.cpp
@@ -5,53 +5,51 @@
 using namespace pqxx;
 using namespace std;
 Select::Select(string selectParams){
-	selectString = selectParams;
-//	notFinished = true;
-}
-
-string Select::resultingString(){
-
-	return resultString;
+    selectString = selectParams;
+    //    notFinished = true;
 }
 
 void Select::queryDatabase(){
-	try{
-	string sql = selectString;
+    try{
+        string sql = selectString;
         cout<<sql<<endl;
-	connection C(connectionString);
-	if (C.is_open()) {
-       	 cout << "Opened database successfully: " << C.dbname() << endl;
-      } 
-	else {
-       	 cout << "Can't open database" << endl;
-      }
-  /* Create a non-transactional object. */
-      nontransaction N(C);
-
-      /* Execute SQL query */
-      result R= N.exec(sql);
-      resultString = "";
-      for(result::const_iterator row = R.begin(); row!=R.end(); row++){
-          for(int i=0;i<row.size();i++){
-              cout<<row[i].as<string>()<<endl;
-  	 	}
-      }
-	
-      cout<<"Operation done successfully!" << endl;
-      C.disconnect();
-      }catch(const exception &e){
-       	cerr<< e.what() << endl;
-	}
-//	notFinished = false;
+        connection C(connectionString);
+        if (C.is_open()) {
+            cout << "Opened database successfully: " << C.dbname() << endl;
+        }
+        else {
+            cout << "Can't open database" << endl;
+        }
+        /* Create a non-transactional object. */
+        nontransaction N(C);
+        
+        /* Execute SQL query */
+        result R= N.exec(sql);
+        int j=0;
+        for(result::const_iterator row = R.begin(); row!=R.end(); row++){
+            j++;
+            for(int i=0;i<row.size();i++){
+                if(!row.at(i).is_null())
+                    cout<<R.column_name(i)<<":"<<row[i].as<string>()<<endl;
+            }
+            cout<<"Data "<<j<<" complete"<<"\n"<<endl;
+        }
+        
+        cout<<"Operation done successfully!" << endl;
+        C.disconnect();
+    }catch(const exception &e){
+        cerr<< e.what() << endl;
+    }
+    //    notFinished = false;
 }
 
 
 void Select::setConnectionParameters(string dbname,string account, string password, string endpoint, int port){
-	string template1 = "dbname=";
-	string template2 = " user=";
-	string template3 = " password=";
-	string template4 = " hostaddr=";
-	string template5 = " port=";
-	connectionString =template1+dbname+template2+account+template3+password+template4+endpoint+template5+to_string(port);
+    string template1 = "dbname=";
+    string template2 = " user=";
+    string template3 = " password=";
+    string template4 = " hostaddr=";
+    string template5 = " port=";
+    connectionString =template1+dbname+template2+account+template3+password+template4+endpoint+template5+to_string(port);
 }
 
--- Select.h
+++ Select.h
@@ -4,17 +4,15 @@ using namespace std;
 using namespace pqxx;
 
 class Select{
-	public:
-		Select(string  selectParams);
-		string resultingString();
-		void setConnectionParameters(string dbname,string account, string password,string endpoint, int port);
-		//bool notFinished;
-		void queryDatabase();
-
-	private:
-		string selectString;
-		string resultString;
-		string connectionString;
-		
-
+public:
+    Select(string  selectParams);
+    void setConnectionParameters(string dbname,string account, string password,string endpoint, int port);
+    //bool notFinished;
+    void queryDatabase();
+    
+private:
+    string selectString;
+    string connectionString;
+    
+    
 };
