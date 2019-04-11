29e3c986f6d09431644fea8524d13607fb32da38
Moynihsu <moynihsu@gmail.com>
2019-04-11 14:05:59 +0100

    Baseline per shift

--- BaselineOver5.py
+++ BaselineOver5.py
@@ -7,11 +7,6 @@ from datetime import datetime
 import numpy as np
 
 
-# select avg(duration) from tevent where date(started_time) between '2018-10-05' and '2018-10-11' and (date_part('hour', started_time) < 19 and date_part('hour', started_time) >= 8) and ward_you_are_working_on_today='IPD1_under_5'
-
-# select avg(duration) from tevent where week, day, shift, ward
-
-
 week0 = "date(started_time) between '2018-10-05' and '2018-10-11' "
 week1 = "date(started_time) between '2018-10-12' and '2018-10-18' "
 week2 = "date(started_time) between '2018-10-19' and '2018-10-25' "
@@ -35,9 +30,6 @@ day_shift = "(date_part('hour', started_time) < 19 and date_part('hour', started
 night_shift = "(date_part('hour', started_time) >= 19 or date_part('hour', started_time) < 8)"
 
 def get_date(week, day):
-    #week = str(week)
-    #day = str(day)
-    
     if week == 3:
         if day<=5:
             date = 12 + day
@@ -46,27 +38,27 @@ def get_date(week, day):
             date = day - 5
             return "started_time between '2018-11-0" + str(date) +  " 00:00:00' and '2018-11-0" + str(date) + " 23:59:59' "
 
-        elif week == 0:
-            date = 5 + day
-            if date<10:
-                return "started_time between '2018-10-0" + str(date) +  " 00:00:00' and '2018-10-0" + str(date) + " 23:59:59' "
-            else:
-                return "started_time between '2018-10-" + str(date) +  " 00:00:00' and '2018-10-" + str(date) + " 23:59:59' "
-        elif week == 1:
-            date = 12 + day
-            if date<10:
-                return "started_time between '2018-10-0" + str(date) +  " 00:00:00' and '2018-10-0" + str(date) + " 23:59:59' "
-            else:
-                return "started_time between '2018-10-" + str(date) +  " 00:00:00' and '2018-10-" + str(date) + " 23:59:59' "
-        elif week == 2:
-            date = 19 + day
-            if date<10:
-                return "started_time between '2018-10-0" + str(date) +  " 00:00:00' and '2018-10-0" + str(date) + " 23:59:59' "
-            else:
-                return "started_time between '2018-10-" + str(date) +  " 00:00:00' and '2018-10-" + str(date) + " 23:59:59' "
-
+    elif week == 0:
+        date = 5 + day
+        if date<10:
+            return "started_time between '2018-10-0" + str(date) +  " 00:00:00' and '2018-10-0" + str(date) + " 23:59:59' "
         else:
-            print("error")
+            return "started_time between '2018-10-" + str(date) +  " 00:00:00' and '2018-10-" + str(date) + " 23:59:59' "
+    elif week == 1:
+        date = 12 + day
+        if date<10:
+            return "started_time between '2018-10-0" + str(date) +  " 00:00:00' and '2018-10-0" + str(date) + " 23:59:59' "
+        else:
+            return "started_time between '2018-10-" + str(date) +  " 00:00:00' and '2018-10-" + str(date) + " 23:59:59' "
+    elif week == 2:
+        date = 19 + day
+        if date<10:
+            return "started_time between '2018-10-0" + str(date) +  " 00:00:00' and '2018-10-0" + str(date) + " 23:59:59' "
+        else:
+            return "started_time between '2018-10-" + str(date) +  " 00:00:00' and '2018-10-" + str(date) + " 23:59:59' "
+
+    else:
+        print("error")
 
 # set up connection to server
 conn = psycopg2.connect("host=nurses.c1sjehtxbob6.eu-west-1.rds.amazonaws.com port=5432 dbname=nurses user=nursesadmin password=password")
@@ -81,10 +73,6 @@ def baselineFunc(day_and_date, shift, ward):
     sum = cur.fetchone()
     sum = sum[0]
     cur.execute("SELECT duration, event_type FROM tevent WHERE " + day_and_date + " AND " + shift + " AND " + ward)
-    #                + " AND " \
-    #                + admission + " OR " + discharge + " OR " + drug_round + " OR " + vital_sign_round + " OR " + patient_documentation + \
-    #                " OR " + meeting_patients_basic_needs + " OR " + handover + " OR " + surveillance + " OR " + taking_a_break + " OR " +\
-    #                indirect_patient_care + " OR " + pharmacy_tasks)
     l = cur.fetchall()
     baseline_sum = 0
     for i in range(0, len(l)):
@@ -100,23 +88,8 @@ def baselineFunc(day_and_date, shift, ward):
             or l[i][1] == 'indirect_patient_care'\
             or l[i][1] == 'pharmacy_tasks':
                 baseline_sum += l[i][0]
-    #    print(sum)
-#    print(baseline_sum)
     baseline = (baseline_sum/sum) * 100
-    #    print(baseline)
     return baseline
-#
-#select avg(duration) from tevent where started_time between '2018-10-05 00:00:00' and '2018-10-05 23:59:59' AND ward_you_are_working_on_today='IPD1_under_5' AND date_part('hour', started_time) >= 19 or date_part('hour', started_time) < 8;
-
-#
-#week00night = get_date(0,0)
-#print("Week0, day0, night, under 5: ", baselineFunc(week00night, night_shift, under_5), "%")
-#week00day = get_date(0,0)
-#print("Week0, day0, night, under 5: ", baselineFunc(week00day, day_shift, under_5), "%")
-#
-#print()
-#week3day0night = get_date(3,0)
-#print("Week3, day0, night, under 5: ", baselineFunc(week3day0night, night_shift, under_5))
 
 weeks = 4
 days = 7
@@ -128,9 +101,7 @@ for i in range(0,weeks):
         baselinePart.append(baselineFunc(get_date(i,j), night_shift, over_5))
         reactivePart.append(100-(baselineFunc(get_date(i,j), day_shift, over_5)))
         reactivePart.append(100-(baselineFunc(get_date(i,j), night_shift, over_5)))
-#print(baselinePart)
-#print()
-#print(reactivePart)
+
 
 # close the communication with the PostgreSQL
 conn.commit()
@@ -140,12 +111,13 @@ conn.close()
 
 ind = np.arange(len(baselinePart))
 width = 0.4
-p1 = plt.bar(ind, baselinePart, color='blue')
-p2 = plt.bar(ind, reactivePart, bottom=baselinePart, color='black')
-plt.ylabel('Percentage of Time')
+p1 = plt.bar(ind, baselinePart, color='#75bbfd')
+p2 = plt.bar(ind, reactivePart, bottom=baselinePart, color='red')
+plt.ylabel('Percentage of Time', fontweight='bold')
+plt.xlabel('Shift', fontweight='bold')
 plt.title('Time Spent on Baseline vs Reactive Care (Over 5 Ward)')
-plt.xticks(ind, ('D', 'N', 'D', 'N', 'D','N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N','D', 'N',))
-#plt.yticks(np.arange(0, 81, 10))
+plt.xticks(ind, ('Day', 'Night', 'Day', 'Night', 'Day','Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night','Day', 'Night'), rotation='vertical')
+plt.yticks(np.arange(0, 101, 10))
 plt.legend((p1[0], p2[0]), ('Baseline', 'Reactive'))
 
 plt.show()
